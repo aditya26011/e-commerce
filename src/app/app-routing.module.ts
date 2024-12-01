3bc26e57f8d2cdf13aceab7325891e0c3c08bd63
Aditya26011 <adityapawar826@gmail.com>
2024-12-01 10:40:09 +0530

    routing and components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { SaleComponent } from './Components/sale/sale.component';
+import { CartComponent } from './Components/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'sale',
+    component: SaleComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { CartComponent } from './Components/cart/cart.component';
+import { SaleComponent } from './Components/sale/sale.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    SaleComponent
   ],
   imports: [
     BrowserModule,
